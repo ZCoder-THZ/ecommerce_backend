dd82cb80e1e062d3a8ae87f20630808b1a65c559
thanhtikezaw <thanhtikezaw919@gmail.com>
2025-05-05 21:48:36 +0700

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,19 @@ import { Server } from "socket.io";
 import router from "./routes/router";
 import { openAPIRouter } from "./api-docs/openAPIRouter";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, // if you need to allow cookies/credentials
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] // allowed methods
+};
 
+// ✅ Use CORS middleware before other middleware
+app.use(cors(corsOptions));
 
 // ✅ Use body-parser
 app.use(bodyParser.json({ limit: '100mb' }));
@@ -16,6 +25,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/docs', openAPIRouter);
 app.use(router);
+
+
 
 // Prisma graceful shutdown
 process.on('SIGINT', async () => {
